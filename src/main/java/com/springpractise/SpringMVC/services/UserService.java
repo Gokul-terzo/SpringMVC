47838d36cd205d755e57e5a97879e3a340d1fddd
Gokul1909 <gokul@terzocloud.com>
2023-07-02 15:11:37 +0530

    Completed till Register

--- src/main/java/com/springpractise/SpringMVC/services/UserService.java
+++ src/main/java/com/springpractise/SpringMVC/services/UserService.java
@@ -1,7 +1,12 @@
 package com.springpractise.SpringMVC.services;
 
 import com.springpractise.SpringMVC.dto.RegistrationDto;
+import com.springpractise.SpringMVC.models.UserEntity;
 
 public interface UserService {
     void saveUser(RegistrationDto registrationDto);
+
+    UserEntity findByEmail(String email);
+
+    UserEntity findByUsername(String username);
 }
