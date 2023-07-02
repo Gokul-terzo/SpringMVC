2eb20faff7372cc84fc3239e28771d1dede3232a
Gokul1909 <gokul@terzocloud.com>
2023-07-02 11:02:23 +0530

    Completed till event-edit

--- src/main/java/com/springpractise/SpringMVC/controller/ClubController.java
+++ src/main/java/com/springpractise/SpringMVC/controller/ClubController.java
@@ -57,8 +57,9 @@ public class ClubController {
     @PostMapping("/clubs/{clubId}/edit")
     public String updateClub(@PathVariable("clubId") int clubId,
                              @Valid @ModelAttribute("club") ClubDto club,
-                             BindingResult result){
+                             BindingResult result,Model model){
         if(result.hasErrors()){
+            model.addAttribute("club",club);
             return "clubs-edit";
         }
         club.setId(clubId);
--- src/main/java/com/springpractise/SpringMVC/controller/EventController.java
+++ src/main/java/com/springpractise/SpringMVC/controller/EventController.java
@@ -1,12 +1,15 @@
 package com.springpractise.SpringMVC.controller;
 
+import com.springpractise.SpringMVC.dto.ClubDto;
 import com.springpractise.SpringMVC.dto.EventDto;
 import com.springpractise.SpringMVC.models.Event;
 import com.springpractise.SpringMVC.services.EventService;
 import jakarta.persistence.GeneratedValue;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,4 +53,28 @@ public class EventController {
         eventService.createEvent(clubId,eventDto);
         return "redirect:/clubs/"+clubId;
     }
+
+    @GetMapping("/events/{eventId}/edit")
+    public String editEventForm(@PathVariable("eventId")Long eventId,Model model){
+        EventDto event=eventService.findByEventId(eventId);
+        model.addAttribute("event",event);
+        return "events-edit";
+    }
+
+    @PostMapping("/events/{eventId}/edit")
+    public String updateEvent(@PathVariable("eventId") Long eventId,
+                             @Valid @ModelAttribute("club") EventDto event,
+                             BindingResult result,Model model){
+        if(result.hasErrors()){
+            model.addAttribute("event",event);
+            return "events-edit";
+        }
+        EventDto eventDto=eventService.findByEventId(eventId);
+
+        event.setId(eventId);
+        event.setClub(eventDto.getClub());
+        eventService.updateEvent(event);
+        return "events-list";
+    }
+
 }
